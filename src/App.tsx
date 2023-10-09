f3af134ac5c41182ffc978d82c7f24ce09a8f0bd
Wagner Terry <goncalveswagner15@gmail.com>
2023-10-09 19:59:15 -0300

    style: estilizando navbar

--- src/App.tsx
+++ src/App.tsx
@@ -18,10 +18,10 @@ function App() {
 
         <nav>
           <ul>
-            <li><a href="#home">Home</a></li>
-            <li><a href="#news">News</a></li>
-            <li><a href="#contact">Contact</a></li>
-            <li><a href="#about">About</a></li>
+            <li><a href="#home">Sobre</a></li>
+            <li><a href="#news">Projetos</a></li>
+            <li><a href="#contact">Skills</a></li>
+            <li><a href="#about">Contato</a></li>
           </ul>
         </nav>
 
